e3f108f2f229b4631a2e1a879c3d3e7d15e3b270
Alex Kan <lexokan@gmail.com>
2020-08-18 21:59:01 -0400

    Move checklist to separate file and add assimilate script

---
+++ assimilate.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+
+set -ex
+
+PREFIX="$HOME"
+DOTFILES="$PREFIX/dotfiles"
+BACKUPS="$PREFIX/backups"
+
+function sym () {
+  src="$DOTFILES/$1"
+  dest="$PREFIX/$2"
+
+  if [ -e "$dest" ]; then
+    newdest="$BACKUPS/$(basename $dest)-$(date +%s)"
+    mv "$dest" "$newdest"
+    echo "> Moved $dest to $newdest"
+  fi
+
+  ln -s "$src" "$dest"
+}
+
+if [ ! -e "$DOTFILES" ]; then
+  echo "error: dotfiles/ needs to reside in $PREFIX"
+  exit 1
+fi
+
+mkdir -p "$PREFIX"
+mkdir -p "$BACKUPS/vim_backups"
+
+sym bashrc              .bashrc
+sym bash_profile        .bash_profile
+sym gitconfig           .gitconfig
+sym tmux.conf           .tmux.conf
+sym zshrc               .zshrc
+sym karabiner           .config/karabiner
+sym nvim                .config/nvim
+sym hammerspoon         .hammerspoon
+sym code_settings.json  .vscode/settings.json
+
+echo "> Assimilation successful!"
+
