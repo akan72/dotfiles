56cc803e4b86c770d077f67dffeb5f191afe3464
Alex Kan <29241719+akan72@users.noreply.github.com>
2023-11-26 15:07:48 -0500

    Add additional remaps

--- nvim/lua/config/remaps.lua
+++ nvim/lua/config/remaps.lua
@@ -1,31 +1,46 @@
+-- Map <leader> to space
+vim.g.mapleader = " "
+vim.g.localleader = " "
+
 local function map(mode, key, val)
-	vim.keymap.set(mode, key, val, { silent = true })
+    vim.keymap.set(mode, key, val, { noremap = true, silent = true })
 end
+
+-- Indent with Tab
+map("n", "<Tab>", ">>")
+
+-- Dedent with Shift + Tab
+map("n", "<S-Tab>", "<<")
 
 -- Clear highlighting by presssing <leader><leader>
 map("n", "<leader><leader>", ":noh<cr>")
 
--- Append and prepend blank lines with <leader>?<Enter>
-map("n", "<Enter>", ":call append(line('.'), '')<CR>")
-map("n", "<leader><Enter>", ":call append(line('.')-1, '')<CR>")
+-- Append blank lines with Enter. Prevent comments from continuing
+map("n", "<Enter>", "o<ESC>S<ESC>")
 
--- Navigate between panes with movement keys
+-- Prepend blank lines with Shift + Enter. Prevent comments from continuing
+map("n", "<S-Enter>", "O<ESC>S<ESC>")
+
+-- Navigate between panes with vim movement keys
 map("n", "<leader>h", ":wincmd h<CR>")
 map("n", "<leader>j", ":wincmd j<CR>")
 map("n", "<leader>k", ":wincmd k<CR>")
 map("n", "<leader>l", ":wincmd l<CR>")
 
--- Move highlighted groups with indents
-map("v", "J", ":m '>+1<CR>gv=gv")
-map("v", "K", ":m '<-2<CR>gv=gv")
-
 -- Center cursor when doing half-page jumps
 map("n", "<C-d>", "<C-d>zz")
-map("n", "<C->", "<C-u>zz")
+map("n", "<C-u>", "<C-u>zz")
+
+-- Search helpdocs for word under cursor
+map("n", "<leader>gh", ":h <C-R>=expand('<cword>')<CR><CR>")
 
 -- Center current search result to middle of screen
 map("n", "n", "nzzzv")
-map("n", "N", "nzzzv")
+map("n", "N", "Nzzzv")
+
+-- Move highlighted groups in visual mode (with indents)
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Preserve copied text after paste
 map("x", "<leader>p", "\"dP")
